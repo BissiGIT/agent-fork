93b102e3d433757cc6b0d72b45faa2f68b2f04fd
Bissiking <34923959+Bissiking@users.noreply.github.com>
2022-06-05 20:45:42 +0200

    0.0.2 - B101

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 // CONST
 const { fork } = require("child_process");
 const fs = require("fs");
-const path = 'modules/agent-website';
-const gitPullOrClone = require('git-pull-or-clone');
-// VAR
 
-function Fork() {
-
+// FUNCTION
+function WriteChildData(apps, idChild, statut) {
+    // Remplissage des informations dans le fichier JSON
+    var dataFork = {
+            id: apps,
+            node_number: idChild,
+            statut: statut,
+            date_statut: new Date(),
+        }
+        // Conversion
+    const forkinfoJSON = JSON.stringify(dataFork);
+    const forkinfoObject = JSON.parse(forkinfoJSON);
+    // Converting js object into JSON string
+    // and writting to data.json file
+    const dataJSON = JSON.stringify(forkinfoObject);
+    // Ecriture du fichier
+    fs.writeFileSync("./data/fork_data/" + apps + "_child.json", dataJSON);
 }
+
+function ForkReboot(apps, data) {
+    // Checkup du nombre de test effectué
+    setTimeout(function() {
+        ForkInit(apps, data);
+    }, 5000);
+}
+
+function Fork(apps) {
+    WriteChildData(apps, idChild, 1);
+
+    child.send();
+    // En cas de fermeture du NODE ---
+    child.on("close", function(code) {
+        console.log("Alerte - Module arrêté || Code: " + code);
+        WriteChildData(apps, idChild, 0);
+        ForkReboot(apps);
+    });
+}
+
+module.exports = { Fork }
